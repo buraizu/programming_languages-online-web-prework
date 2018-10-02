b7106026eedec945d165ea53885e938deacc3660
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-02 02:36:56 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  return_hash = {}
+  languages.each do |key, value|
+    value.each do |key2, value2|
+      value2.each do |key3, value3|
+        return_hash[key2] = {"#{key3}" => "#{value3}", "style =>": "#{key.to_s.split}"}
+      end
+    end
+  end
+  return return_hash
 end
